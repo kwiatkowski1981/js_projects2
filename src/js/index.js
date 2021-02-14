5dc7d0b493a91a6b696193b32311c97c1e9f6d8d
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-14 12:57:58 +0100

    updating game counters, depending on values passed to functions. I add printouts for the player about the result, color the text of the result.

--- src/js/index.js
+++ src/js/index.js
@@ -53,9 +53,23 @@ function checkResult(player, ai) {
 function publishResult(player, ai, result) {
 document.querySelector('[data-summary="your-choice"]').textContent = player;
 document.querySelector('[data-summary="ai-choice"]').textContent = ai;
-document.querySelector('[data-summary="who-win"]').textContent = result;
+// document.querySelector('[data-summary="who-win"]').textContent = result;
 // gameSummary.numbers++
     document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+    if (result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = "Zwycięstwo";
+        document.querySelector('[data-summary="who-win"]').style.color = "green";
+    }else if (result === "loss") {
+        document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+        document.querySelector('[data-summary="who-win"]').textContent = "Porażka";
+        document.querySelector('[data-summary="who-win"]').style.color = "red";
+    }else {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = "Remis";
+        document.querySelector('[data-summary="who-win"]').style.color = "black";
+    }
+
 }
 
 
